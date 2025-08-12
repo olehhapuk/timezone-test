f7a8f947ec62609a57d657e6613ea4d733678a27
olehhapuk <oleggapuk20@gmail.com>
2025-08-12 16:52:37 +0300

    test

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,6 @@ const { integer, pgTable, timestamp } = require('drizzle-orm/pg-core');
 const testTable = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   date: timestamp('date', {
-    withTimezone: true,
     mode: 'string',
   }).notNull(),
 });
